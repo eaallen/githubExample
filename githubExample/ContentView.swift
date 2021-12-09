25d161db3a4b95414e2527b3820795686fa680c1
Elijah Allen <elijah.a.allen@gmail.com>
2021-12-08 21:38:58 -0700

    Initial Commit

---
+++ githubExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubExample
+//
+//  Created by Gove Allen on 12/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubExample/githubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubExampleApp.swift
+//  githubExample
+//
+//  Created by Gove Allen on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct githubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
